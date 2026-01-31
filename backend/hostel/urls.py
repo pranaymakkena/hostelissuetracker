ed99b5089685d3ee8f08a71d0b7aa41fdfddc167
Pranay Makkena <pranaymakkena@gmail.com>
2026-01-31 14:29:57 +0530

    Update urls.py

--- backend/hostel/urls.py
+++ backend/hostel/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # API routes
     path('api/users/', include('users.urls')),
     path('api/issues/', include('issues.urls')),
+
+    # health check (optional)
+    path('', lambda r: HttpResponse("Backend Running ✅")),
 ]
 
+# media files (images)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
